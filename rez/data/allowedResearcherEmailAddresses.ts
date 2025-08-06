3a8c2bd4c663b5b9a9a28a7dc3082f7c37d48421
andrewkimjoseph <andrewkimjoseph@gmail.com>
2025-08-06 21:34:05 +0300

    feat(auth): add new researcher email address
    - Add benwrites20@gmail.com as Researcher Three to allowed email addresses

--- rez/data/allowedResearcherEmailAddresses.ts
+++ rez/data/allowedResearcherEmailAddresses.ts
@@ -2,4 +2,5 @@ export const allowedResearcherEmailAddresses = [
   "thecanvassing@gmail.com", // Researcher Zero
   "waystrecycling@gmail.com", // Researcher One
   "kimathidennis38@gmail.com", // Researcher Two
+  "benwrites20@gmail.com", // Researcher Three
 ];
